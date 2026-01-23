8bb0624a1713cc5fe971db5bc0f6f092d18cbece
David Mismas <david.mismas@gmail.com>
2026-01-23 11:13:56 +0100

    working version, with rotation, vertical lines, sound, haptics, welcome view and ui changes. ready to delpoy.

---
+++ TetrixPuzzle/Game/WelcomeView.swift
@@ -0,0 +1,8 @@
+//
+//  WelcomeView.swift
+//  TetrixPuzzle
+//
+//  Created by David Mišmaš on 23. 1. 26.
+//
+
+import Foundation
---
+++ TetrixPuzzle/Pieces/ClearShatterOverlay.swift
@@ -0,0 +1,8 @@
+//
+//  ClearShatterOverlay.swift
+//  TetrixPuzzle
+//
+//  Created by David Mišmaš on 23. 1. 26.
+//
+
+import Foundation
